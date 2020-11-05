4f68de7989ae4fd8b3d09aadd78f5c04bc0e48eb
Cédric <cedric.rosat@heig-vd.ch>
2020-11-05 12:02:21 +0100

    v1.1

---
+++ bissextiles.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cedri on 05.11.2020.
+//
+
+#include "bissextiles.h"
---
+++ bissextiles.h
@@ -0,0 +1,8 @@
+//
+// Created by cedri on 05.11.2020.
+//
+
+#ifndef LABO3_BISSEXTILES_H
+#define LABO3_BISSEXTILES_H
+
+#endif //LABO3_BISSEXTILES_H
