a470f4c03eafa9f161aacb35d1cedae43ef53843
Cédric <cedric.rosat@heig-vd.ch>
2020-11-10 10:38:50 +0100

    Ajout de controleSaisie au programme.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,59 @@
 /*
 -----------------------------------------------------------------------------------
 Nom du fichier  : main.cpp
-Auteur(s)       : Ian Escher, Crotti Pablo, Rosat Cédric
-Date creation   : 05.11.2020
+Auteur(s)       : Crotti Pablo, Escher Ian, Rosat Cédric
+Date creation   : 06.11.2020
 
-Description     : <à compléter>
+Description     : Fichier principal du programme Labo3
 
 Remarque(s)     : <à compléter>
 
 Compilateur     : Mingw-w64 g++ 8.1.0
 -----------------------------------------------------------------------------------
 */
-#include <cstdlib>
+#include "controleSaisie.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-   //test
-   cout << "Helloooooo";
-   return EXIT_SUCCESS;
+    const unsigned int MOIS_MIN = 0, MOIS_MAX = 12;
+    const unsigned int ANNEE_MIN = 1900, ANNEE_MAX = 2100;
+
+    bool programmeActif = true;
+    do {
+        bool saisieDate;
+        unsigned int moisDebut, anneeDebut, moisFin, anneeFin;
+        do {
+            // Pas d'erreur en début de boucle
+            saisieDate = true;
+
+            string messageErreur = "Date non valide. Veuillez SVP recommencer.";
+            string messageEntree = "Veuillez entrer la date de debut [mm aaaa] :";
+            controleSaisie(MOIS_MIN, MOIS_MAX, ANNEE_MIN, ANNEE_MAX, &moisDebut,
+                           &anneeDebut, messageEntree, messageErreur);
+            messageEntree = "Veuillez entrer la date de fin [mm aaaa]";
+            controleSaisie(MOIS_MIN, MOIS_MAX, ANNEE_MIN, ANNEE_MAX, &moisFin,
+                           &anneeFin, messageEntree, messageErreur);
+
+            // Contrôle la cohérence des dates
+            if (anneeDebut > anneeFin) saisieDate = false;
+            if (anneeDebut == anneeFin) {
+                if (moisDebut > moisFin) saisieDate = false;
+            }
+
+            cout << moisDebut << "/" << anneeDebut << " - "
+                 << moisFin << "/" << anneeFin << endl;
+        } while (!saisieDate);
+
+        char nouvelleSaisie;
+        cout << "Voulez-vous quitter le programme ? [o/n] :";
+        cin >> nouvelleSaisie;
+        if (nouvelleSaisie == 'o') programmeActif = true;
+        if (nouvelleSaisie == 'n') programmeActif = false;
+    } while (!programmeActif);
+
+    return EXIT_SUCCESS;
+}
 }
